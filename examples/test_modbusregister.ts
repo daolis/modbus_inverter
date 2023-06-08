7fd7a96448372dd2ac52c818d90d5bad91dbaf3d
Patrick Wichmann <patrick.wichmann@gmx.de>
2023-06-08 22:02:29 +0200

    change Filename

---
+++ examples/test_modbusregister.ts
@@ -0,0 +1,25 @@
+/**
+ * Test Modbusregister to implement 
+ */
+
+import { HuaweiSDongleA } from "../index";
+let ipAddress = "192.168.178.26";
+let modbusPort = 502;
+let unitId = 1;
+const device = new HuaweiSDongleA(ipAddress, modbusPort, unitId);
+
+async function readMB(){
+    await console.log("Connect to: " + device.getIpAddress());
+    let modell = await device.getModell();
+    let modellId = await device.getModellID();
+    let serialNr = await device.getSerialNumber();
+    let unknown = await device.getUnknown();
+    await console.log("Modell: " + modell);
+    await console.log("ModellID: " + modellId);
+    await console.log("Seriennummer: " + serialNr);
+    await console.log("unknown: " + unknown);
+    
+    await device.close();
+    return;
+}
+readMB();
